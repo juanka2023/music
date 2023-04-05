400d091e4d90af4f20c15eea9cefc7f83b7ba909
juanka2023 <bull-ter@hotmail.es>
2023-04-05 13:59:38 +0200

    inicio de programacion

--- app/src/main/java/com/example/music/MainActivity.java
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.music;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1, et2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.etdatos);
+        et2 = (EditText) findViewById(R.id.contenido);
+    }
+    //metodo guardad
+    public void guardar(View view){
+        String nombre = et1.getText().toString();
+        String contenido = et2.getText().toString();
+
+        try {
+            File tarjetaSD = Environment.getExternalStorageDirectory();
+            Toast.makeText(this, tarjetaSD.getPath(), Toast.LENGTH_SHORT).show();
+        }catch (IOException e){
     }
 }
